24f08bc9cc83e2c38a84637cdf2bb99c9ffbc0cb
webSeungKyu <abcsksk1@gmail.com>
2024-04-01 16:01:15 +0900

    미사일 발사 시 효과음 추가

--- Assets/Script/Launcher.cs
+++ Assets/Script/Launcher.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class Launcher : MonoBehaviour
 {
     public GameObject bullet; // 미사일 프리팹 가져올 변수
+    public AudioClip shotSound;
+    private AudioSource soundPlayer;
     void Start()
     {
+        soundPlayer = GetComponent<AudioSource>();
         //InvokeRepeating ( 함수 이름, 초기지연 시간, 지연할 시간 )
         InvokeRepeating("Shoot", 1f, 0.42f);
+
     }
 
     void Shoot()
     {
         //미사일 프리팹, 런쳐포지션, 방향값 없음
         Instantiate(bullet, transform.position, Quaternion.identity);
+
+        if (soundPlayer != null)
+        {
+            Debug.Log("Shot Sound Play!");
+            soundPlayer.PlayOneShot(shotSound);
+        }
+        else
+        {
+            Debug.Log("Shot Sound NoPlay!");
+        }
 
     }
     void Update()
