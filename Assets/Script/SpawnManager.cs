8baea05c20240fa21065e5c6284376a38d326605
webSeungKyu <abcsksk1@gmail.com>
2024-04-01 15:43:57 +0900

    List<GameObject)를 이용하여 적 생성

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public bool enableSpawn = false; // 생성 On/Off
-    public GameObject enemy; // prefab으로 만든 적을 가져온다
+    //public GameObject enemy; // prefab으로 만든 적을 가져온다
+    public List<GameObject> enemyList;
 
     public void SpawnEnemy()
     {
+        /* 랜덤 생성
         float randomX = Random.Range(-2f, 2f); // 적이 나타날 x좌표를 랜덤으로 생성한다.
 
         if (enableSpawn)
@@ -16,12 +18,22 @@ public class SpawnManager : MonoBehaviour
             //적을 생성한다. randomX랜덤한 값
             Instantiate(enemy, new Vector3(randomX, transform.position.y, 0f), Quaternion.identity);
         }
+        */
+        if (enableSpawn)
+        {
+            for(int i = 0; i < enemyList.Count; i++)
+            {
+                Instantiate(enemyList[i], new Vector2(transform.position.x + i, transform.position.y), Quaternion.identity);
+            }
+        }
+
+
     }
 
 
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", 1, 0.5f);
+        InvokeRepeating("SpawnEnemy", 1, 3f);
     }
 
 
