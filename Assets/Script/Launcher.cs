739e65a8162ab46daf715449cb43395d633bb739
webSeungKyu <abcsksk1@gmail.com>
2024-04-01 13:48:15 +0900

    미사일 발사 (bullet) 구현
    OnBecameInvisible
    화면 밖으로 나가면 안 보일경우 호출이 되는 함수로 자동 삭제

---
+++ Assets/Script/Launcher.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject bullet; // 미사일 프리팹 가져올 변수
+    void Start()
+    {
+        //InvokeRepeating ( 함수 이름, 초기지연 시간, 지연할 시간 )
+        InvokeRepeating("Shoot", 0.5f, 1f);
+    }
+
+    void Shoot()
+    {
+        //미사일 프리팹, 런쳐포지션, 방향값 없음
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float moveSpeed = 0.45f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Y축 이동
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+    }
+
+    //화면 밖으로 나가면 안 보일경우 호출이 되는 함수 (Ctrl + Shift + M) 눌러서 찾을 수도 있음
+    private void OnBecameInvisible()
+    {
+        //미사일 지우기 > 직접 보고 있는 카메라에서 안 보일 경우에 삭제됨 (테스트로 Scene 넓게 보면 안 사라짐)
+        Destroy(gameObject);
+    }
+}
